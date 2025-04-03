32d6453413bf326164dddea0a56ed7827dbf068a
Cedric <cedric.jamme@edu.devinci.fr>
2025-04-03 11:42:50 +0200

    add vercel and vite config

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    outDir: 'dist',
+    assetsInlineLimit: 0,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+  },
+  publicDir: 'assets',
+  server: {
+    fs: {
+      allow: ['..']
+    }
+  },
+  resolve: {
+    alias: {
+      '@assets': resolve(__dirname, 'assets')
+    }
+  }
+});
